c8d4eef6b131db8689f5635e926400a3570ab939
Bayane Chakir <bayane.pbg@gmail.com>
2025-03-14 16:55:16 +0100

    making a randmoiser when cards clicked

--- main.js
+++ main.js
@@ -1,35 +1,48 @@
-
-// remove div 
-// loop show ?? cards as the input is 
-// if card is clicked use randomiser for half of the card 
-// show the card for 3 sec 
-// another if, if cards are the same number 
-// make them disappear
-// add + 2 score  
-// when cards are matched replace the background color 
-
-
-
 const container = document.querySelector("#memoryCards");
 container.classList.add("container");
+
 container.querySelector(".memoryCard").remove();
 
-document.getElementById("inputNbrOfCard").value;
 const input = document.getElementById("inputNbrOfCard");
+const valueInput = input.value;
 
 const btn = document.getElementById("btnRestart");
-btn.addEventListener('click', onClick);
+document.getElementById("btnRestart").innerHTML = "start"; 
 
-const valueInput = input.value;
-function onClick() {
 
-    for (i = 0; i < valueInput; i++) {
+btn.addEventListener("click", backCard());
+
+function backCard() {
+
+for (i = 0; i < 10; i++) {
 
         let cards = document.createElement("div");
-        cards.textContent = "??"
+        cards.textContent = "??";
         container.appendChild(cards);
         cards.classList.add("card");
-    }
 
+ 
+}
 }
 
+function returnedCard() {
+
+        cards.addEventListener("click", () => onClick);
+        let rnd =  Math.floor(Math.random() * 10);
+        console.log(rnd);
+        if (cards.clicked == true) {
+         cards.innerHTML = rnd;
+
+}
+}
+
+
+
+
+
+// while (numbersArr.length > 0) { 
+//     let cards = document.createElement("div");
+//     cards.textContent = "??";
+//     let numbersArr = Array.from({ length: 10 }, (_, i) => i + 1); 
+//     const currentNumber = numbers.splice(10, rnd);
+//     let rnd =  Math.floor(Math.random() * numbersArr);
